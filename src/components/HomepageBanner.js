081880959c3de22991944d13223f1334e06327c6
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-12-02 12:51:57 +0000

    add avatars to HomepageBanner

--- src/components/HomepageBanner.js
+++ src/components/HomepageBanner.js
@@ -2,17 +2,27 @@ import React from "react";
 import { RichText } from "prismic-reactjs";
 
 const HomepageBanner = (props) => {
-    return (
-        <div>
-            {props.doc ? (
-                <>
+    const showContent = (props) => {
+        if (props.doc) {
+            const imgs = Object.keys(props.doc.data.avatars[0]).map((key) => {
+                return props.doc.data.avatars[0][key];
+            });
+            return (
+                <div>
                     <h1>{RichText.asText(props.doc.data.page_title)}</h1>
-                </>
-            ) : (
-                <div>No content</div>
-            )}
-        </div>
-    );
+                    <div>
+                        {imgs.map((img, i) => (
+                            <img key={i} src={img.url} />
+                        ))}
+                    </div>
+                </div>
+            );
+        } else {
+            return <div>No content</div>;
+        }
+    };
+
+    return <>{showContent(props)}</>;
 };
 
 export default HomepageBanner;
